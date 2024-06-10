9f380fd85d26490655782371b772470d2805d2f2
Muhammad AbdulSalam <muhammadabdulsalam@Muhammads-MacBook-Pro.local>
2024-06-10 20:58:17 +0100

    updated loop for data tables

--- src/test/java/com/skylink/automation/ContactUsPageTest.java
+++ src/test/java/com/skylink/automation/ContactUsPageTest.java
@@ -1,49 +1,59 @@
 package com.skylink.automation;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.List;
+
 /**
  * Anything in RED means an error - code will not run
  * Yellow lines - which are the warnings - code will run bit its good to fix them
  * Green line - mean a typo or wrong spellings - code will run
- *
+ * <p>
  * Cannot resolve symbol 'Given'
- *
+ * <p>
  * We need to define cucumber options
  * These are to customise your cucumber testing
- *
+ * <p>
  * Reports will be generated in target folder
- *
+ * <p>
  * Data table -
  */
 @RunWith(JUnit4.class)
 public class ContactUsPageTest {
 
-    @Given("User is on skyline website")
-    public void user_is_on_skyline_website() {
+    @Given("User can press submit button")
+    public void user_can_press_submit_button() {
 
     }
 
-    @Then("User clicks on contacts us button")
-    public void user_clicks_on_contacts_us_button() {
+    /**
+     * String result = dataTable.cell(2,2);
+     * System.out.println("this is my cell value: " + result);
+     * get() means get me element at position X
+     * List<String> myRowNumberOne = dataTable.row(1);
+     * Practise
+     */
+    @Then("Press submit button with following details")
+    public void press_submit_button_with_following_details(DataTable dataTable) {
+
+        /**
+         * Counter - a variable to keep how many times loop have run
+         * Condition - how many times to run it -  when to stop - run until
+         * increment - step
+         */
+
+        for(int counter = 0; counter < 8;  counter++){
+            System.out.println("counter value now is : " + counter);
+
+            List<String> row = dataTable.row(counter);
+
+            System.out.println("printing counter: " + row);
+        }
 
     }
-
-//    And Press submit button with following details
-//    |salam  |salam@test.com|
-//            |second |second@Test.com|
-//            |third  |third@test.com |
-//            # horizontal is a row
-//  # vertical is a column
-//    @Then("Press submit button with following details")
-//    public void press_submit_button_with_following_details(io.cucumber.datatable.DataTable dataTable) {
-//        dataTable.height(); // number of rows
-//        dataTable.width(); // how many column
-//        dataTable.row(0); // get me row number at index
-//        dataTable.column(0); // get me column number at index
-//    }
 }
