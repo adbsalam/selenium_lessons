cb64f9ad0b171cb6661449987bc3fdda2e0c5711
Muhammad AbdulSalam <muhammadabdulsalam@Muhammads-MacBook-Pro.local>
2024-11-27 20:41:55 +0000

    lesson 4 assertions

--- src/test/java/com/salam/firstLesson/MyChromeDriverTest.java
+++ src/test/java/com/salam/firstLesson/MyChromeDriverTest.java
@@ -8,50 +8,27 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.ArrayList;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class MyChromeDriverTest {
 
-    // initialisation -> new Something();
-    // -> ( Constructors - Java Objects - initialise a class) -> read about these
     ChromeDriver chromeDriver = new ChromeDriver();
 
-    /**
-     * Web drivers are tools that help you automate a browser
-     * chrome driver - chrome automation
-     * edge driver - edge automation
-     * firefox driver - firefox automation
-     */
     @Before
-    public void setup() {
+    public void setup() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "/Users/muhammadabdulsalam/Dev/intelij/driver/chromedriver");
 
-        chromeDriver.get("https://admin.staging.skylinkimpact.com");
+        chromeDriver.get("https://automationexercise.com");
+        Thread.sleep(2000);
     }
 
-    /**
-     * Everything on your website is a web element
-     * Always find the IDs -
-     * xpath -> location of an item on the website/screen
-     *
-     * sleep will be in milli seconds which is 1s = 1000ms
-     *
-     * Try to complete login -
-     * verify that user can login after entering email and password
-     */
     @Test
-    public void myFirstClickTest() throws InterruptedException {
-        WebElement continueButton = chromeDriver.findElement(By.xpath("/html/body/div[1]/main/div[1]/div[2]/div/div/div/div/a"));
-
-        continueButton.click();
-
+    public void testMyTopNavigationMenu() throws InterruptedException {
+        WebElement productsMenu = chromeDriver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[2]/a"));
+        productsMenu.click();
         Thread.sleep(2000);
 
-        WebElement emailText = chromeDriver.findElement(By.xpath("//*[@id=\"undefined-:r2:\"]"));
-
-        emailText.sendKeys("test.test@gmail.com");
-
-        Thread.sleep(2000);
-
-        chromeDriver.quit();
+        assertEquals("https://automationexercise.com/products", chromeDriver.getCurrentUrl());
     }
 
 }
