5492dd1d604ddc94612a8053db062c8f5abc0276
Muhammad AbdulSalam <muhammadabdulsalam@Muhammads-MacBook-Pro.local>
2024-12-03 21:01:09 +0000

    cucumber setup

--- src/test/java/com/salam/firstLesson/MyChromeDriverTest.java
+++ src/test/java/com/salam/firstLesson/MyChromeDriverTest.java
@@ -1,18 +1,7 @@
 package com.salam.firstLesson;
 
-import io.cucumber.java.bs.A;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-
-import java.util.ArrayList;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Loops
@@ -20,27 +9,15 @@ import static org.junit.Assert.assertTrue;
  * Loop is repeating code
  */
 public class MyChromeDriverTest {
-    //initialize  Browser
-    ChromeDriver chromeDriver = new ChromeDriver();
 
-    // Common  starting steps
-    @Before
-    public void setUp() throws InterruptedException {
-        ArrayList<String> dummyData = new ArrayList<>();
+    @Test
+    public void LaunchBrowser(){
 
-        dummyData.add("//*[@id=\"first_name\"]");
-        dummyData.add("//*[@id=\"last_name\"]");
-        dummyData.add("//*[@id=\"company\"]");
-        dummyData.add("//*[@id=\"address1\"]");
-        dummyData.add("//*[@id=\"address2\"]");
-        dummyData.add("//*[@id=\"country\"]");
-        dummyData.add("//*[@id=\"country\"]");
+    }
 
-        for (String xpath : dummyData) {
-            WebElement firstNameText = chromeDriver.findElement(By.xpath(xpath));
-            firstNameText.sendKeys("lorum");
-            Thread.sleep(2000);
-        }
+    @Test
+    public void navigateToSomething(){
+
     }
 
 
---
+++ src/test/resources/features/MyFirstFeature.feature
@@ -0,0 +1,6 @@
+Feature: Register User
+  Scenario: Successful registration
+    Given User should be on automation website home page
+    Then Verify that home page is visible successfully
+    Then  Click on Signup / Login button
+
